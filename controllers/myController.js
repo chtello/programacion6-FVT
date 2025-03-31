86c0734844ef14130a684defce9f11987da17cd0
chtelo <chtello@escuelasproa.edu.ar>
2025-03-31 00:10:55 -0300

    pagina lista falta el contenido

--- controllers/myController.js
+++ controllers/myController.js
@@ -2,3 +2,6 @@
 exports.inicio = (req, res) => {
     res.status(200).render('index'); 
 };
+exports.datos = (req, res) => {
+    res.status(200).render('datos'); 
+};
--- routes/myRouter.js
+++ routes/myRouter.js
@@ -3,5 +3,5 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/').get(myController.inicio); 
-
+router.route('/Datos').get(myController.datos); 
 module.exports = router;
